670394d3cda3c14aea9ca67f666b55c0fac202ac
Alexandre Gérault <gerault-alexandre@orange.fr>
2025-04-28 18:04:56 +0200

    feat(nvim): add default neotree configuration

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,12 +1,16 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    window = { position = "right" },
+  },
   branch = "v3.x",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  config = function()
+  config = function(_, opts)
     vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal right<CR>")
-  end
+    require("neo-tree").setup(opts)
+  end,
 }

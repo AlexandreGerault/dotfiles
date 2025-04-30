d11603d846868bcf063e4fa2dcd37a8926a9a163
Alexandre Gérault <alexandre@hexium.io>
2025-04-30 20:16:24 +0200

    feat(nvim): install go lsp

--- nvim/lua/plugins/lsp-configuration.lua
+++ nvim/lua/plugins/lsp-configuration.lua
@@ -15,15 +15,16 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
-          "lua_ls",
-          "ts_ls",
-          "eslint",
-          "dockerls",
-          "docker_compose_language_service",
-          "tailwindcss",
-          "clangd",
           "biome",
+          "clangd",
+          "docker_compose_language_service",
+          "dockerls",
+          "eslint",
+          "gopls",
+          "lua_ls",
           "rust_analyzer",
+          "tailwindcss",
+          "ts_ls",
           "yamlls"
         },
       })

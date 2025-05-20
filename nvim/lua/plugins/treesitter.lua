1393cae81c2bd0ec0089d02f10a1f64bc28b8dda
Alexandre Gérault <alexandre@hexium.io>
2025-05-20 20:54:48 +0200

    feat(nvim): add go to treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "lua", "javascript", "cpp", "php", "rust" },
+      ensure_installed = { "lua", "javascript", "cpp", "php", "rust", "go" },
       highlight = { enable = true },
       indent = { enable = true },
     })
